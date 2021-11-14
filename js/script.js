b4f2d7b802b8c3900dc75786b2c5de3369ecc77a
takayuki <dettiseed@gmail.com>
2021-11-14 18:44:36 +0900

    アコーディオン追加前

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
+/* ハンバーガーメニュー */
 let menu_btn = document.getElementById('hamburger-btn');
 let header_nav = document.getElementById('header-nav');
 
 menu_btn.addEventListener('click' , function () {
     header_nav.classList.toggle('menu-open');
   } );
+
+/* アコーディオン */
+const accordion_menu = document.querySelectorAll(".js-menu");
+
+function toggle() {
+  const content = this.nextElementSibling;
+  /* this.classList.toggle("is-active"); */
+  content.classList.toggle("content-open");
+}
+
+for (let i = 0; i < menu_btn.clientHeight; i++) {
+  accordion_menu[i].addEventListener("click", toggle);
+}
