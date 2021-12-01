f56970e0c54a246d02d2e2e65b5e0790cba4abc1
takayuki <dettiseed@gmail.com>
2021-12-01 11:38:56 +0900

    制作実績完成

---
+++ js/my-swiper.js
@@ -0,0 +1,27 @@
+/* Initialize Swiper */
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    centeredSlides : true,
+    initialSlide: 10,
+    autoplay: {
+        delay: 3000,
+    },
+
+    spaceBetween: 5,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+
+    breakpoints: {
+        416: {
+            slidesPerView: 2,
+        },
+        1000: {
+            slidesPerView: 3,
+        },
+        1400: {
+            slidesPerView: 4,
+        },
+    }
+});
