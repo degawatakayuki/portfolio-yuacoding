f476d3477d42f9f16655aeeb2754dd486df40785
takayuki <dettiseed@gmail.com>
2022-05-30 10:58:06 +0900

    フォーム　バリデーションテストBK

--- js/my-form.js
+++ js/my-form.js
@@ -12,9 +12,14 @@ function update() {
 
     /* チェックが入っている場合ボタンを有効 */
     if (isRequired) {
-        form_button.disabled = false;
+        /* form_button.disabled = false; */
         form_button.style.opacity = 1;
         form_button.style.cursor = "pointer";
+
+
+
+
+        
     } else {
         form_button.disabled = true;
         form_button.style.opacity = 0.6;
